b013ff0986683733ec580c359332e5dbc6fa953b
znezniV <vleutenegger@gmail.com>
2018-07-04 22:56:08 +0200

    added youtube iframe api

---
+++ script.js
@@ -0,0 +1,51 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        videoId: video.id,
+        playerVars: {
+            modestbranding: true,
+            autoplay: 1,
+            rel: 0,
+            modestbranding: true,
+            autohide: 1,
+            showinfo: 0,
+            controls: 0,
+            playsinline: 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+      event.target.a.classList.add('luminanceToAlpha');
+      event.target.playVideo();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(player.videoId = 'x-zq9FBPQRI', 6000);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
