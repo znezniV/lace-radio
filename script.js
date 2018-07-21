8e18fd33411189437e7c1786e577d4a3811c2916
znezniV <vleutenegger@gmail.com>
2018-07-21 21:53:38 +0200

    configure player

--- script.js
+++ script.js
@@ -85,27 +85,28 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '100%',
-        width: '100%',
-        videoId: video.id,
-        playerVars: {
-            modestbranding: true,
-            autoplay: 1,
-            rel: 0,
-            modestbranding: true,
-            autohide: 1,
-            showinfo: 0,
-            controls: 0,
-            playsinline: 1
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
+
+player = new YT.Player('player', {
+    height: '100%',
+    width: '100%',
+    videoId: 'NxUTashBrAY',
+    playerVars: {
+        modestbranding: true,
+        autoplay: 1,
+        rel: 0,
+        modestbranding: true,
+        autohide: 1,
+        showinfo: 0,
+        controls: 0,
+        playsinline: 1,
         fs: 0,
         iv_load_policy: 3
+    },
+    events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+    }
+});
 }
 
   // 4. The API will call this function when the video player is ready.
