f69fe585ad9f64b08a91c93ea0df476cc4eed2e2
znezniV <vleutenegger@gmail.com>
2018-07-21 21:51:29 +0200

    add function to shuffle data array

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ request.onload = function() {
         var resp = request.responseText;
 
         data = parseData(resp);
+        shuffleArray(data);
 
         let randomVideo = getRandomID(data);
 
@@ -51,6 +52,24 @@ function parseData(data) {
 function getRandomID(data) {
     let randomItem = data[Math.floor(Math.random() * data.length)];
     return randomItem;
+}
+
+function shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
 }
 
 // 2. This code loads the IFrame Player API code asynchronously.
